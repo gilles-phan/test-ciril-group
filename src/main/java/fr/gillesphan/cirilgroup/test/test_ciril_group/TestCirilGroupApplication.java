876472937059ace1f3c437d6dcc96f9594102920
Gilles Phan (macbook) <phan.gilles@gmail.com>
2025-09-24 15:48:57 +0200

    - import from spring initializr

---
+++ src/main/java/fr/gillesphan/cirilgroup/test/test_ciril_group/TestCirilGroupApplication.java
@@ -0,0 +1,13 @@
+package fr.gillesphan.cirilgroup.test.test_ciril_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCirilGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCirilGroupApplication.class, args);
+	}
+
+}
